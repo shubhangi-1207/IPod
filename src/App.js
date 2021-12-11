2354de4aec6f637886e09d2fde8c1c190553af71
Shubhangi Aggarwal <77059662+shubhangi-1207@users.noreply.github.com>
2021-12-11 10:36:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ipod from './components/Ipod';
+
+function App() {
+  return (
+    <div className="App">
+      <Ipod />
+    </div>
+  );
+}
+
+export default App;
